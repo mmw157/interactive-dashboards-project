5aa4fc252021e137d531302002c8a421472bff9c
Ashley Ptinis <aptinis@gmail.com>
2026-04-16 10:57:05 -0400

    Prelim app for exploration

---
+++ init_app.py
@@ -0,0 +1,31 @@
+import marimo
+
+__generated_with = "0.23.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import ipyniivue
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
